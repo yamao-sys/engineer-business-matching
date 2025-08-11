aa283903981c6500ae48ca3fe800157350e8c59a
yamao-sys <yamaopanku@gmail.com>
2025-08-11 10:46:08 +0900

    Business APIのCompanyの登録エンドポイントを追加

--- api-server/app/controllers/api/v1/business/companies_controller.rb
+++ api-server/app/controllers/api/v1/business/companies_controller.rb
@@ -1,9 +1,36 @@
 class Api::V1::Business::CompaniesController < Api::V1::Business::BaseController
   def validate_sign_up
-    render json: { message: "Hello, world!" }
+    permitted_params = validate_sign_up_params
+    company = Company.new(name: permitted_params[:name], email: permitted_params[:email], password: permitted_params[:password])
+    company.final_tax_return = permitted_params[:final_tax_return]
+
+    if company.valid?
+      render json: { errors: {} }
+    else
+      render json: { errors: format_errors(company.errors) }, status: :bad_request
+    end
   end
 
   def sign_up
-    render json: { message: "Hello, world!" }
+    permitted_params = sign_up_params
+    company = Company.new(name: permitted_params[:name], email: permitted_params[:email], password: permitted_params[:password])
+    company.final_tax_return = permitted_params[:final_tax_return]
+
+    if company.invalid?
+      render json: { errors: format_errors(company.errors) }, status: :bad_request
+    else
+      company.save!
+      render json: { errors: {} }
+    end
+  end
+
+  private
+
+  def validate_sign_up_params
+    params.permit(:name, :email, :password, :final_tax_return).to_h.symbolize_keys
+  end
+
+  def sign_up_params
+    params.permit(:name, :email, :password, :final_tax_return).to_h.symbolize_keys
   end
 end
---
+++ api-server/spec/requests/api/v1/business/companies_controller_spec.rb
@@ -0,0 +1,100 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Api::V1::Business::CompaniesController, type: :request do
+  describe "POST #validate_sign_up" do
+    include_context "with files"
+
+    let(:params) { { name:, email:, password:, final_tax_return: } }
+
+    subject { post validate_sign_up_api_v1_business_companies_url, params: }
+
+    context "when the params are valid" do
+      let(:name) { "test" }
+      let(:email) { "test@example.com" }
+      let(:password) { "password" }
+      let(:final_tax_return) { sample_jpg_file }
+
+      it "returns 200" do
+        aggregate_failures do
+          expect(subject).to eq(200)
+
+          res_body = JSON.parse(response.body).deep_symbolize_keys
+          expect(res_body[:errors]).to eq({})
+        end
+      end
+    end
+
+    context "when the params are invalid" do
+      context "when the name is blank" do
+        let(:name) { "" }
+        let(:email) { "test@example.com" }
+        let(:password) { "password" }
+        let(:final_tax_return) { sample_jpg_file }
+
+        it "returns 400" do
+          aggregate_failures do
+            expect(subject).to eq(400)
+
+            res_body = JSON.parse(response.body).deep_symbolize_keys
+            expected_errors = { name: [ "企業名を入力してください。" ] }
+            expect(res_body[:errors]).to eq(expected_errors)
+          end
+        end
+      end
+    end
+  end
+
+  describe "POST #sign_up" do
+    include_context "with files"
+
+    let(:params) { { name:, email:, password:, final_tax_return: } }
+
+    subject { post sign_up_api_v1_business_companies_url, params: }
+
+    context "when the params are valid" do
+      let(:name) { "test" }
+      let(:email) { "test@example.com" }
+      let(:password) { "password" }
+      let(:final_tax_return) { sample_jpg_file }
+
+      it "returns 200" do
+        aggregate_failures do
+          expect(subject).to eq(200)
+
+          res_body = JSON.parse(response.body).deep_symbolize_keys
+          expect(res_body[:errors]).to eq({})
+
+          created_company = Company.find_by(email:)
+          expect(created_company.name).to eq(name)
+          expect(created_company.email).to eq(email)
+          expect(created_company.password_digest).to be_present
+          expect(created_company.final_tax_return.metadata["filename"]).to eq("sample.jpg")
+        end
+      end
+    end
+
+    context "when the params are invalid" do
+      context "when the name is blank" do
+        let(:name) { "" }
+        let(:email) { "test@example.com" }
+        let(:password) { "password" }
+        let(:final_tax_return) { sample_jpg_file }
+
+        it "returns 400" do
+          aggregate_failures do
+            expect(subject).to eq(400)
+
+            res_body = JSON.parse(response.body).deep_symbolize_keys
+            expected_errors = { name: [ "企業名を入力してください。" ] }
+            expect(res_body[:errors]).to eq(expected_errors)
+
+            created_company = Company.find_by(email:)
+            expect(created_company).to be_nil
+          end
+        end
+      end
+    end
+  end
+end
