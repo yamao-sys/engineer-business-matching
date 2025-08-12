6bdbe4ad814aab7a6ce29ecd46ff9c0386233a6f
yamao-sys <yamaopanku@gmail.com>
2025-08-12 17:08:58 +0900

    Businessのsign_upページの作成

---
+++ frontend/apps/business/features/signUp/components/SignUpInput/index.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { FC, useState } from "react";
+import { PhaseType } from "@/features/signUp/types";
+import { CompanySignUpInput, CompanySignUpValidationError } from "@/apis/model";
+import { useFormContext } from "react-hook-form";
+import BaseControlFormInput from "@repo/ui/BaseControlFormInput/index";
+import BaseButton from "@repo/ui/BaseButton/index";
+import { validateSignUp } from "../../actions/signUp";
+import BaseControlFormImage from "@repo/ui/BaseControlFormImage/index";
+
+type Props = {
+  togglePhase: (newPhase: PhaseType) => void;
+};
+
+const INITIAL_VALIDATION_ERRORS = {
+  name: [],
+  email: [],
+  password: [],
+  finalTaxReturn: [],
+};
+
+const SignUpInput: FC<Props> = ({ togglePhase }: Props) => {
+  const [validationErrors, setValidationErrors] = useState<CompanySignUpValidationError>(INITIAL_VALIDATION_ERRORS);
+
+  const { control, handleSubmit, setValue, watch } = useFormContext<CompanySignUpInput>();
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      // TODO: loadingをTanstack Queryで管理する
+      // setLoading(true);
+      const resValidationErrors = await validateSignUp(data);
+      if (Object.keys(resValidationErrors).length > 0) {
+        setValidationErrors(resValidationErrors);
+        return;
+      }
+
+      // NOTE: 確認画面に遷移
+      togglePhase("confirm");
+    } catch (error) {
+      console.error("Failed to save draft:", error);
+      alert("送信に失敗しました");
+    }
+  });
+
+  return (
+    <>
+      <h3 className="mt-16 w-full text-center text-2xl font-bold">企業登録フォーム</h3>
+
+      <form onSubmit={onSubmit}>
+        <div className="mt-8">
+          <BaseControlFormInput id="name" label="企業名" control={control} name="name" validationErrors={validationErrors.name ?? []} />
+        </div>
+
+        <div className="mt-8">
+          <BaseControlFormInput
+            id="email"
+            label="メールアドレス"
+            control={control}
+            name="email"
+            type="email"
+            validationErrors={validationErrors.email ?? []}
+          />
+        </div>
+
+        <div className="mt-8">
+          <BaseControlFormInput
+            id="password"
+            label="パスワード"
+            control={control}
+            name="password"
+            type="password"
+            validationErrors={validationErrors.password ?? []}
+          />
+        </div>
+
+        <div className="mt-8">
+          <BaseControlFormImage
+            id="finalTaxReturn"
+            label="確定申告書(コピー)"
+            control={control}
+            setValue={setValue}
+            watch={watch}
+            name="finalTaxReturn"
+            validationErrors={validationErrors.finalTaxReturn ?? []}
+          />
+        </div>
+
+        <div className="flex justify-end space-x-4 pt-6 border-t border-gray-200">
+          <BaseButton borderColor="border-green-500" bgColor="bg-green-500" label="確認画面へ" type="submit" />
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default SignUpInput;
---
+++ frontend/apps/business/features/signUp/components/SignUpLayout/index.tsx
@@ -0,0 +1,36 @@
+import { FC, ReactNode } from "react";
+import { PhaseType } from "@/features/signUp/types";
+import BaseContainer from "@repo/ui/BaseContainer/index";
+
+type Props = {
+  phase: PhaseType;
+  children: ReactNode;
+};
+
+const SignUpLayout: FC<Props> = ({ phase, children }: Props) => {
+  return (
+    <div className="p-4 md:p-16">
+      <BaseContainer containerWidth="w-4/5">
+        <div className="flex justify-between mb-16">
+          <div>
+            <span className={phase === "input" ? "text-blue-300" : "text-gray-300"}>登録情報の入力</span>
+          </div>
+          <div className="text-gray-300">&gt;&gt;</div>
+
+          <div>
+            <span className={phase === "confirm" ? "text-blue-300" : "text-gray-300"}>登録情報の確認</span>
+          </div>
+          <div className="text-gray-300">&gt;&gt;</div>
+
+          <div>
+            <span className={phase === "thanks" ? "text-blue-300" : "text-gray-300"}>登録完了</span>
+          </div>
+        </div>
+
+        {children}
+      </BaseContainer>
+    </div>
+  );
+};
+
+export default SignUpLayout;
