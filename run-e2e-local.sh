3a4ffb989ab7bba1edb03559383154cc373b2c8c
yamao-sys <yamaopanku@gmail.com>
2025-08-12 16:58:19 +0900

    ローカルでE2Eができるように

---
+++ run-e2e-local.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# シーディング
+# docker compose exec -d api_server make test-seed-local
+
+# テスト用の環境でAPIサーバを起動
+docker compose exec -d api_server rails s -e test -p 4100 -b '0.0.0.0'
+
+# テスト実行
+docker compose exec frontend pnpm test:e2e
+
+# テスト用DBのリセット
+docker compose run --rm db mysql -h db -u root -p -e "DROP DATABASE engineer_business_matching_test; CREATE DATABASE engineer_business_matching_test;"
+docker compose run --rm api_server sh -c 'RAILS_ENV=test rails ridgepole:apply'
