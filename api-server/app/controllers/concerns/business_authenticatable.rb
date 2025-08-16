a71f12acadcba7c9c6e76f70846663cc5885e442
yamao-sys <yamaopanku@gmail.com>
2025-08-16 17:13:15 +0900

    Business APIのJWTトークン署名のアルゴリズムを変更

--- api-server/app/controllers/concerns/business_authenticatable.rb
+++ api-server/app/controllers/concerns/business_authenticatable.rb
@@ -8,7 +8,7 @@ module BusinessAuthenticatable
     else
       token = authorization_header.split(" ")[1]
       secret_key = Rails.application.credentials.secret_key_base
-      decoded_token = JWT.decode(token, secret_key)
+      decoded_token = JWT.decode(token, secret_key, true, { algorithm: "HS256" })
 
       @company = Company.find(decoded_token[0]["company_id"])
     end
@@ -17,6 +17,6 @@ module BusinessAuthenticatable
   def business_create_token(company_id)
     payload = { company_id:, exp: (Time.current + 1.days).to_i }
     secret_key = Rails.application.credentials.secret_key_base
-    JWT.encode(payload, secret_key)
+    JWT.encode(payload, secret_key, "HS256")
   end
 end
