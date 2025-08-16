dc8805a361ec1d9decdcfbae8972db7ed8aafbd8
yamao-sys <yamaopanku@gmail.com>
2025-08-16 17:15:43 +0900

    UIパーツの拡充

---
+++ frontend/packages/ui/src/BaseErrorMessage/index.tsx
@@ -0,0 +1,16 @@
+import { memo } from "react";
+
+type Props = {
+  message: string;
+  additionalClassName?: string;
+};
+
+const BaseErrorMessage = memo(function BaseErrorMessage({ message, additionalClassName }: Props) {
+  return (
+    <>
+      <p className={`text-red-400 ${additionalClassName}`}>{message}</p>
+    </>
+  );
+});
+
+export default BaseErrorMessage;
