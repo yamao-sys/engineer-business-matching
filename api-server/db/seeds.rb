08d63f1b070b02d0ea1287748b2a78256c78cf3f
yamao-sys <yamaopanku@gmail.com>
2025-08-17 17:56:25 +0900

    TalentのログインページのE2E作成

--- api-server/db/seeds.rb
+++ api-server/db/seeds.rb
@@ -1,1 +1,3 @@
 Company.create!(name: "test_company_sign_in", email: "test_company_sign_in@example.com", password: "password")
+
+Engineer.create!(first_name: "test_engineer_sign_in", last_name: "test_engineer_sign_in", email: "test_engineer_sign_in@example.com", password: "password", birthday: "1990-01-01")
