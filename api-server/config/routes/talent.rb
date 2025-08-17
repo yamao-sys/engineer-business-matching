5039bf23be3febf6dd0fbcba241eec0f2b7377b0
yamao-sys <yamaopanku@gmail.com>
2025-08-17 11:30:54 +0900

    Talent APIのログイン エンドポイント追加

--- api-server/config/routes/talent.rb
+++ api-server/config/routes/talent.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
           collection do
             post "validate-sign-up", to: "engineers#validate_sign_up"
             post "sign-up", to: "engineers#sign_up"
+            post "sign-in", to: "engineers#sign_in"
           end
         end
       end
