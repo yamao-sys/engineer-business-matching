e8088312864af247a5938457bc32709180355d71
yamao-sys <yamaopanku@gmail.com>
2025-08-23 22:44:17 +0900

    localstackのcors対応

--- docker/localstack/init.sh
+++ docker/localstack/init.sh
@@ -2,3 +2,20 @@
 
 awslocal s3 mb s3://engineer-business-matching-dev
 awslocal s3 mb s3://engineer-business-matching-test
+
+# CORS設定を追加
+aws --endpoint-url=http://localhost:4566 \
+  s3api put-bucket-cors \
+  --no-sign-request \
+  --bucket engineer-business-matching-dev \
+  --cors-configuration '{
+    "CORSRules": [
+        {
+            "AllowedHeaders": ["*"],
+            "AllowedMethods": ["GET", "PUT", "POST"],
+            "AllowedOrigins": ["http://localhost:3000", "http://localhost:3001"],
+            "ExposeHeaders": ["Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers"],
+            "MaxAgeSeconds": 3000
+        }
+    ]
+}'
