27a99708138ad5cf8b7be6a7f545b8749042390d
yamao-sys <yamaopanku@gmail.com>
2025-08-23 12:27:35 +0900

    Business Profile updateアクションの実装

--- api-server/app/controllers/api/v1/business/profiles_controller.rb
+++ api-server/app/controllers/api/v1/business/profiles_controller.rb
@@ -4,4 +4,22 @@ class Api::V1::Business::ProfilesController < Api::V1::Business::BaseController
   def show
     render json: Serializer.call(CompanySerializer, @company)
   end
+
+  def update
+    permitted_params = update_params
+    @company.assign_attributes(permitted_params)
+
+    if @company.invalid?
+      render json: { errors: format_errors(@company.errors), company: Serializer.call(CompanySerializer, @company) }, status: :bad_request
+    else
+      @company.save!
+      render json: { errors: {}, company: Serializer.call(CompanySerializer, @company) }
+    end
+  end
+
+  private
+
+  def update_params
+    params.permit(:name, :logo, :email, :address, :siteUrl, :employeeCount, :industryId).to_h.transform_keys(&:underscore).symbolize_keys
+  end
 end
--- api-server/spec/requests/api/v1/business/profiles_controller_spec.rb
+++ api-server/spec/requests/api/v1/business/profiles_controller_spec.rb
@@ -29,4 +29,38 @@ RSpec.describe Api::V1::Business::ProfilesController, type: :request do
       end
     end
   end
+
+  describe "PUT #update" do
+    include_context "with files"
+    include_context "with business auth"
+
+    let(:industry) { Industry.find_by(name: "コミュニーケーション系") }
+    let!(:company) { create(:company, email: "test@example.com", password: "password", industry:, logo: sample_jpg_file) }
+
+    subject { put api_v1_business_profile_url, params: { name: "test", logo: Rack::Test::UploadedFile.new(Rails.root.join("spec/fixtures/sample.jpg")), email: "test@example.com", address: "test", siteUrl: "test", employeeCount: 1, industryId: industry.id }, headers: business_auth_header }
+
+    it "returns 200" do
+      aggregate_failures do
+        expect(subject).to eq(200)
+
+        res_body = JSON.parse(response.body).deep_symbolize_keys
+        expect(res_body[:errors]).to eq({})
+        expect(res_body[:company][:id]).to eq(company.id)
+      end
+    end
+
+    context "when the params are invalid" do
+      subject { put api_v1_business_profile_url, params: { name: "", logo: Rack::Test::UploadedFile.new(Rails.root.join("spec/fixtures/sample.jpg")), email: "test@example.com", address: "test", siteUrl: "test", employeeCount: 1, industryId: industry.id }, headers: business_auth_header }
+
+      it "returns 400" do
+        aggregate_failures do
+          expect(subject).to eq(400)
+
+          res_body = JSON.parse(response.body).deep_symbolize_keys
+          expect(res_body[:errors]).to be_present
+          expect(res_body[:company]).to be_present
+        end
+      end
+    end
+  end
 end
