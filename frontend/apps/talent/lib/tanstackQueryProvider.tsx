8e8a7d9e47f6d57c08dd3becf137ca325fe2bf21
yamao-sys <yamaopanku@gmail.com>
2025-08-24 17:31:26 +0900

    TalentにTanstackQueryProvider追加

---
+++ frontend/apps/talent/lib/tanstackQueryProvider.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState, type ReactNode } from "react";
+
+interface TanstackQueryProviderProps {
+  children: ReactNode;
+}
+
+function TanstackQueryProvider({ children }: TanstackQueryProviderProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 10 * 60 * 1000, // 10分
+            gcTime: 15 * 60 * 1000, // 15分
+            retry: 1,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default TanstackQueryProvider;
