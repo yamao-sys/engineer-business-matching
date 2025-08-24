a6a9ca93c5aa3f2a60a064d0ca4c4d1d031c2364
yamao-sys <yamaopanku@gmail.com>
2025-08-24 11:13:59 +0900

    BusinessのProfile編集のactions作成

---
+++ frontend/apps/business/features/Profile/actions/profile.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import { Profile, ProfileUpdateInput, ProfileUpdateResponse } from "@/apis/model";
+import { getProfile, putProfile } from "@/apis/profile/profile";
+import { cookies } from "next/headers";
+
+export const showProfile = async (): Promise<Profile> => {
+  try {
+    const businessToken = `Bearer ${(await cookies()).get("businessToken")?.value}`;
+    const res = await getProfile({ headers: { "Business-Authorization": businessToken } });
+
+    switch (res.status) {
+      case 200:
+        return res.data;
+      default:
+        throw new Error("Failed to show profile", { cause: res.data });
+    }
+  } catch (error) {
+    console.error("Failed to show profile:", error);
+    throw error;
+  }
+};
+
+export const updateProfile = async (data: ProfileUpdateInput): Promise<ProfileUpdateResponse> => {
+  try {
+    const businessToken = `Bearer ${(await cookies()).get("businessToken")?.value}`;
+    const res = await putProfile(data, { headers: { "Business-Authorization": businessToken } });
+
+    switch (res.status) {
+      case 200:
+        return res.data;
+      case 400:
+        return res.data;
+      default:
+        throw new Error("Failed to update profile");
+    }
+  } catch (error) {
+    console.error("Failed to update profile:", error);
+    throw error;
+  }
+};
