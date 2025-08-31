a258362205c88bfaf5f0faf7b9a7e2f6a99291aa
yamao-sys <yamaopanku@gmail.com>
2025-08-31 12:48:46 +0900

    Businessの/company-productsページを追加

---
+++ frontend/apps/business/app/company-products/page.tsx
@@ -0,0 +1,5 @@
+import CompanyProductsTemplate from "@/features/CompanyProducts/components/CompanyProductsTemplate";
+
+export default function CompanyProducts() {
+  return <CompanyProductsTemplate />;
+}
