898cdd41458bcc0999f9ad3b612d17e5bd7b48f3
yamao-sys <yamaopanku@gmail.com>
2025-08-31 12:58:59 +0900

    Business /company-productsページのE2E追加

--- api-server/db/seeds.rb
+++ api-server/db/seeds.rb
@@ -16,6 +16,24 @@ test_company_company_story_3 = Company.create!(name: "test_company_company_story
 test_company_company_story_4 = Company.create!(name: "test_company_company_story_4", email: "test_company_company_story_4@example.com", password: "password")
 CompanyStory.create!(company: test_company_company_story_4, mission: "test_company_story_mission_4", vision: "test_company_story_vision_4")
 
+# NOTE: /company-productsのページのE2Eデータ
+test_company_company_product = Company.create!(name: "test_company_company_product", email: "test_company_company_product@example.com", password: "password")
+test_company_product_1 = CompanyProduct.create!(company: test_company_company_product, name: "test_company_product_1", description: "test_company_product_1_description", url: "http://example.com/1")
+test_company_product_2 = CompanyProduct.create!(company: test_company_company_product, name: "test_company_product_2", description: "test_company_product_2_description", url: "http://example.com/2")
+test_company_product_3 = CompanyProduct.create!(company: test_company_company_product, name: "test_company_product_3", description: "test_company_product_3_description", url: "http://example.com/3")
+File.open(Rails.root.join("spec/fixtures/sample.jpg"), "r") do |file|
+  test_company_product_1.logo_attacher.assign(file)
+end
+test_company_product_1.save!
+File.open(Rails.root.join("spec/fixtures/sample.jpg"), "r") do |file|
+  test_company_product_2.logo_attacher.assign(file)
+end
+test_company_product_2.save!
+File.open(Rails.root.join("spec/fixtures/sample.jpg"), "r") do |file|
+  test_company_product_3.logo_attacher.assign(file)
+end
+test_company_product_3.save!
+
 Engineer.create!(first_name: "test_engineer_sign_in", last_name: "test_engineer_sign_in", email: "test_engineer_sign_in@example.com", password: "password", birthday: "1990-01-01")
 
 Engineer.create!(first_name: "test_engineer_profile", last_name: "test_engineer_profile", email: "test_engineer_profile@example.com", password: "password", birthday: "1990-01-01")
---
+++ frontend/apps/business/e2e/company-products.spec.ts
@@ -0,0 +1,351 @@
+import { test, expect } from "@playwright/test";
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+test.describe("/company-products", () => {
+  test("正常系_一覧表示", async ({ page }) => {
+    await page.goto("/sign_in");
+
+    // NOTE: フォームを入力
+    await page.getByRole("textbox", { name: "メールアドレス" }).fill("test_company_company_product@example.com");
+    await page.getByRole("textbox", { name: "パスワード" }).fill("password");
+
+    await page.getByRole("button", { name: "ログインする" }).click();
+
+    // NOTE: ログインに成功すると、HOME画面に遷移する
+    await page.waitForURL("/");
+    await expect(page).toHaveURL("/");
+
+    // NOTE: プロダクト画面に遷移する
+    await page.goto("/company-products");
+
+    // NOTE: プロダクト一覧が表示される
+    await expect(page.getByText("プロダクトが登録されていません。")).not.toBeVisible();
+    await expect(page.getByText("編集する")).toHaveCount(3);
+    await expect(page.getByText("削除する")).toHaveCount(3);
+
+    await expect(page.getByAltText("test_company_product_1のロゴ")).toBeVisible();
+    await expect(page.getByAltText("test_company_product_2のロゴ")).toBeVisible();
+    await expect(page.getByAltText("test_company_product_3のロゴ")).toBeVisible();
+  });
+
+  // test("正常系_新規追加", async ({ page }) => {});
+
+  // test("正常系_新規追加して削除", async ({ page }) => {});
+
+  // test("正常系_既存のプロダクトありで新規追加", async ({ page }) => {});
+
+  // test("正常系_既存のプロダクトありで編集", async ({ page }) => {});
+
+  // test("正常系_既存のプロダクトありで削除", async ({ page }) => {});
+
+  // test("正常系_入力_更新", async ({ page }) => {
+  //   await page.goto("/sign_in");
+
+  //   // NOTE: フォームを入力
+  //   await page.getByRole("textbox", { name: "メールアドレス" }).fill("test_company_profile@example.com");
+  //   await page.getByRole("textbox", { name: "パスワード" }).fill("password");
+
+  //   await page.getByRole("button", { name: "ログインする" }).click();
+
+  //   // NOTE: ログインに成功すると、HOME画面に遷移する
+  //   await page.waitForURL("/");
+  //   await expect(page).toHaveURL("/");
+
+  //   // NOTE: プロフィール画面に遷移する
+  //   await page.goto("/profile");
+
+  //   // NOTE: フォームを入力
+  //   await page.getByRole("textbox", { name: "会社名" }).fill("test_company_profile");
+  //   const fileChooserPromise = page.waitForEvent("filechooser");
+  //   await page.getByText("ロゴ ファイルをドラッグ&ドロップ、またはクリックして選択").click();
+  //   const logoFileChooser = await fileChooserPromise;
+  //   await logoFileChooser.setFiles(path.join(path.dirname(fileURLToPath(import.meta.url)), "fixtures/test.jpg"));
+  //   await page.getByRole("textbox", { name: "所在地" }).fill("test_address");
+  //   await page.getByRole("textbox", { name: "サイトURL" }).fill("test@example.com/test_site");
+  //   await page.getByRole("textbox", { name: "従業員数" }).fill("100");
+
+  //   await page.getByRole("button", { name: "更新する" }).click();
+
+  //   page.on("dialog", async (dialog) => {
+  //     expect(dialog.message()).toContain("プロフィールを更新しました。");
+  //     await dialog.accept();
+  //   });
+
+  //   await expect(page.getByRole("textbox", { name: "会社名" })).toHaveValue("test_company_profile");
+  //   await expect(page.getByAltText("プレビュー")).toBeVisible();
+  //   await expect(page.getByRole("textbox", { name: "所在地" })).toHaveValue("test_address");
+  //   await expect(page.getByRole("textbox", { name: "サイトURL" })).toHaveValue("test@example.com/test_site");
+  //   await expect(page.getByRole("textbox", { name: "従業員数" })).toHaveValue("100");
+
+  //   // NOTE: 再びページを訪れると、更新した内容が表示されること
+  //   await page.goto("/profile");
+
+  //   await expect(page.getByRole("textbox", { name: "会社名" })).toHaveValue("test_company_profile");
+  //   await expect(page.getByAltText("プレビュー")).toBeVisible();
+  //   await expect(page.getByRole("textbox", { name: "所在地" })).toHaveValue("test_address");
+  //   await expect(page.getByRole("textbox", { name: "サイトURL" })).toHaveValue("test@example.com/test_site");
+  //   await expect(page.getByRole("textbox", { name: "従業員数" })).toHaveValue("100");
+  // });
+
+  // test("正常系_入力_更新_ロゴ削除(リロードなし)", async ({ page }) => {
+  //   await page.goto("/sign_in");
+
+  //   // NOTE: フォームを入力
+  //   await page.getByRole("textbox", { name: "メールアドレス" }).fill("test_company_profile_2@example.com");
+  //   await page.getByRole("textbox", { name: "パスワード" }).fill("password");
+
+  //   await page.getByRole("button", { name: "ログインする" }).click();
+
+  //   // NOTE: ログインに成功すると、HOME画面に遷移する
+  //   await page.waitForURL("/");
+  //   await expect(page).toHaveURL("/");
+
+  //   // NOTE: プロフィール画面に遷移する
+  //   await page.goto("/profile");
+
+  //   // NOTE: フォームを入力
+  //   await page.getByRole("textbox", { name: "会社名" }).fill("test_company_profile_2_edited");
+  //   const fileChooserPromise = page.waitForEvent("filechooser");
+  //   await page.getByText("ロゴ ファイルをドラッグ&ドロップ、またはクリックして選択").click();
+  //   const logoFileChooser = await fileChooserPromise;
+  //   await logoFileChooser.setFiles(path.join(path.dirname(fileURLToPath(import.meta.url)), "fixtures/test.jpg"));
+  //   await page.getByRole("textbox", { name: "所在地" }).fill("test_address");
+  //   await page.getByRole("textbox", { name: "サイトURL" }).fill("test@example.com/test_site");
+  //   await page.getByRole("textbox", { name: "従業員数" }).fill("100");
+
+  //   await page.getByRole("button", { name: "更新する" }).click();
+
+  //   page.on("dialog", async (dialog) => {
+  //     expect(dialog.message()).toContain("プロフィールを更新しました。");
+  //     await dialog.accept();
+  //   });
+
+  //   await expect(page.getByRole("textbox", { name: "会社名" })).toHaveValue("test_company_profile_2_edited");
+  //   await expect(page.getByAltText("プレビュー")).toBeVisible();
+  //   await expect(page.getByRole("textbox", { name: "所在地" })).toHaveValue("test_address");
+  //   await expect(page.getByRole("textbox", { name: "サイトURL" })).toHaveValue("test@example.com/test_site");
+  //   await expect(page.getByRole("textbox", { name: "従業員数" })).toHaveValue("100");
+
+  //   // NOTE: ロゴを削除する
+  //   await page.getByText("削除").click();
+  //   await page.getByRole("button", { name: "更新する" }).click();
+
+  //   page.on("dialog", async (dialog) => {
+  //     expect(dialog.message()).toContain("プロフィールを更新しました。");
+  //     await dialog.accept();
+  //   });
+
+  //   await expect(page.getByAltText("プレビュー")).not.toBeVisible();
+
+  //   // NOTE: 再びページを訪れると、更新した内容が表示されること
+  //   await page.goto("/profile");
+
+  //   await expect(page.getByRole("textbox", { name: "会社名" })).toHaveValue("test_company_profile_2_edited");
+  //   await expect(page.getByAltText("プレビュー")).not.toBeVisible();
+  //   await expect(page.getByRole("textbox", { name: "所在地" })).toHaveValue("test_address");
+  //   await expect(page.getByRole("textbox", { name: "サイトURL" })).toHaveValue("test@example.com/test_site");
+  //   await expect(page.getByRole("textbox", { name: "従業員数" })).toHaveValue("100");
+  // });
+
+  // test("正常系_入力_更新_ロゴ削除(リロードあり)", async ({ page }) => {
+  //   await page.goto("/sign_in");
+
+  //   // NOTE: フォームを入力
+  //   await page.getByRole("textbox", { name: "メールアドレス" }).fill("test_company_profile_3@example.com");
+  //   await page.getByRole("textbox", { name: "パスワード" }).fill("password");
+
+  //   await page.getByRole("button", { name: "ログインする" }).click();
+
+  //   // NOTE: ログインに成功すると、HOME画面に遷移する
+  //   await page.waitForURL("/");
+  //   await expect(page).toHaveURL("/");
+
+  //   // NOTE: プロフィール画面に遷移する
+  //   await page.goto("/profile");
+
+  //   // NOTE: フォームを入力
+  //   await page.getByRole("textbox", { name: "会社名" }).fill("test_company_profile_3_edited");
+  //   const fileChooserPromise = page.waitForEvent("filechooser");
+  //   await page.getByText("ロゴ ファイルをドラッグ&ドロップ、またはクリックして選択").click();
+  //   const logoFileChooser = await fileChooserPromise;
+  //   await logoFileChooser.setFiles(path.join(path.dirname(fileURLToPath(import.meta.url)), "fixtures/test.jpg"));
+  //   await page.getByRole("textbox", { name: "所在地" }).fill("test_address");
+  //   await page.getByRole("textbox", { name: "サイトURL" }).fill("test@example.com/test_site");
+  //   await page.getByRole("textbox", { name: "従業員数" }).fill("100");
+
+  //   await page.getByRole("button", { name: "更新する" }).click();
+
+  //   page.on("dialog", async (dialog) => {
+  //     expect(dialog.message()).toContain("プロフィールを更新しました。");
+  //     await dialog.accept();
+  //   });
+
+  //   await expect(page.getByRole("textbox", { name: "会社名" })).toHaveValue("test_company_profile_3_edited");
+  //   await expect(page.getByAltText("プレビュー")).toBeVisible();
+  //   await expect(page.getByRole("textbox", { name: "所在地" })).toHaveValue("test_address");
+  //   await expect(page.getByRole("textbox", { name: "サイトURL" })).toHaveValue("test@example.com/test_site");
+  //   await expect(page.getByRole("textbox", { name: "従業員数" })).toHaveValue("100");
+
+  //   // NOTE: 再びページを訪れると、更新した内容が表示されること
+  //   await page.goto("/profile");
+
+  //   await expect(page.getByRole("textbox", { name: "会社名" })).toHaveValue("test_company_profile_3_edited");
+  //   await expect(page.getByAltText("プレビュー")).toBeVisible();
+  //   await expect(page.getByRole("textbox", { name: "所在地" })).toHaveValue("test_address");
+  //   await expect(page.getByRole("textbox", { name: "サイトURL" })).toHaveValue("test@example.com/test_site");
+  //   await expect(page.getByRole("textbox", { name: "従業員数" })).toHaveValue("100");
+
+  //   // NOTE: ロゴを削除する
+  //   await page.getByText("削除").click();
+  //   await page.getByRole("button", { name: "更新する" }).click();
+
+  //   page.on("dialog", async (dialog) => {
+  //     expect(dialog.message()).toContain("プロフィールを更新しました。");
+  //     await dialog.accept();
+  //   });
+
+  //   await expect(page.getByAltText("プレビュー")).not.toBeVisible();
+
+  //   // NOTE: 再びページを訪れると、更新した内容が表示されること
+  //   await page.goto("/profile");
+
+  //   await expect(page.getByRole("textbox", { name: "会社名" })).toHaveValue("test_company_profile_3_edited");
+  //   await expect(page.getByAltText("プレビュー")).not.toBeVisible();
+  //   await expect(page.getByRole("textbox", { name: "所在地" })).toHaveValue("test_address");
+  //   await expect(page.getByRole("textbox", { name: "サイトURL" })).toHaveValue("test@example.com/test_site");
+  //   await expect(page.getByRole("textbox", { name: "従業員数" })).toHaveValue("100");
+  // });
+
+  // test("正常系_入力_更新_ロゴ変更", async ({ page }) => {
+  //   await page.goto("/sign_in");
+
+  //   // NOTE: フォームを入力
+  //   await page.getByRole("textbox", { name: "メールアドレス" }).fill("test_company_profile_4@example.com");
+  //   await page.getByRole("textbox", { name: "パスワード" }).fill("password");
+
+  //   await page.getByRole("button", { name: "ログインする" }).click();
+
+  //   // NOTE: ログインに成功すると、HOME画面に遷移する
+  //   await page.waitForURL("/");
+  //   await expect(page).toHaveURL("/");
+
+  //   // NOTE: プロフィール画面に遷移する
+  //   await page.goto("/profile");
+
+  //   // NOTE: フォームを入力
+  //   await page.getByRole("textbox", { name: "会社名" }).fill("test_company_profile_4_edited");
+  //   const fileChooserPromise = page.waitForEvent("filechooser");
+  //   await page.getByText("ロゴ ファイルをドラッグ&ドロップ、またはクリックして選択").click();
+  //   const logoFileChooser = await fileChooserPromise;
+  //   await logoFileChooser.setFiles(path.join(path.dirname(fileURLToPath(import.meta.url)), "fixtures/test.jpg"));
+  //   await page.getByRole("textbox", { name: "所在地" }).fill("test_address");
+  //   await page.getByRole("textbox", { name: "サイトURL" }).fill("test@example.com/test_site");
+  //   await page.getByRole("textbox", { name: "従業員数" }).fill("100");
+
+  //   await page.getByRole("button", { name: "更新する" }).click();
+
+  //   page.on("dialog", async (dialog) => {
+  //     expect(dialog.message()).toContain("プロフィールを更新しました。");
+  //     await dialog.accept();
+  //   });
+
+  //   await expect(page.getByRole("textbox", { name: "会社名" })).toHaveValue("test_company_profile_4_edited");
+  //   await expect(page.getByAltText("プレビュー")).toBeVisible();
+  //   await expect(page.getByRole("textbox", { name: "所在地" })).toHaveValue("test_address");
+  //   await expect(page.getByRole("textbox", { name: "サイトURL" })).toHaveValue("test@example.com/test_site");
+  //   await expect(page.getByRole("textbox", { name: "従業員数" })).toHaveValue("100");
+
+  //   // NOTE: ロゴを削除する
+  //   await page.getByText("削除").click();
+  //   await page.getByRole("button", { name: "更新する" }).click();
+
+  //   page.on("dialog", async (dialog) => {
+  //     expect(dialog.message()).toContain("プロフィールを更新しました。");
+  //     await dialog.accept();
+  //   });
+
+  //   await expect(page.getByAltText("プレビュー")).not.toBeVisible();
+
+  //   // NOTE: 再びページを訪れると、更新した内容が表示されること
+  //   await page.goto("/profile");
+
+  //   await expect(page.getByRole("textbox", { name: "会社名" })).toHaveValue("test_company_profile_4_edited");
+  //   await expect(page.getByAltText("プレビュー")).not.toBeVisible();
+  //   await expect(page.getByRole("textbox", { name: "所在地" })).toHaveValue("test_address");
+  //   await expect(page.getByRole("textbox", { name: "サイトURL" })).toHaveValue("test@example.com/test_site");
+  //   await expect(page.getByRole("textbox", { name: "従業員数" })).toHaveValue("100");
+
+  //   const otherFileChooserPromise = page.waitForEvent("filechooser");
+  //   await page.getByText("ロゴ ファイルをドラッグ&ドロップ、またはクリックして選択").click();
+  //   const otherLogoFileChooser = await otherFileChooserPromise;
+  //   await otherLogoFileChooser.setFiles(path.join(path.dirname(fileURLToPath(import.meta.url)), "fixtures/test.jpg"));
+
+  //   await page.getByRole("button", { name: "更新する" }).click();
+  //   page.on("dialog", async (dialog) => {
+  //     expect(dialog.message()).toContain("プロフィールを更新しました。");
+  //     await dialog.accept();
+  //   });
+  //   // NOTE: 再びページを訪れると、更新した内容が表示されること
+  //   await page.goto("/profile");
+  //   await expect(page.getByRole("textbox", { name: "会社名" })).toHaveValue("test_company_profile_4_edited");
+  //   await expect(page.getByAltText("プレビュー")).toBeVisible();
+  //   await expect(page.getByRole("textbox", { name: "所在地" })).toHaveValue("test_address");
+  //   await expect(page.getByRole("textbox", { name: "サイトURL" })).toHaveValue("test@example.com/test_site");
+  //   await expect(page.getByRole("textbox", { name: "従業員数" })).toHaveValue("100");
+  // });
+
+  // test("異常系", async ({ page }) => {
+  //   await page.goto("/sign_in");
+
+  //   // NOTE: フォームを入力
+  //   await page.getByRole("textbox", { name: "メールアドレス" }).fill("test_company_profile_5@example.com");
+  //   await page.getByRole("textbox", { name: "パスワード" }).fill("password");
+
+  //   await page.getByRole("button", { name: "ログインする" }).click();
+
+  //   // NOTE: ログインに成功すると、HOME画面に遷移する
+  //   await page.waitForURL("/");
+  //   await expect(page).toHaveURL("/");
+
+  //   // NOTE: プロフィール画面に遷移する
+  //   await page.goto("/profile");
+
+  //   // NOTE: フォームを入力
+  //   await page.getByRole("textbox", { name: "会社名" }).fill("");
+  //   const fileChooserPromise = page.waitForEvent("filechooser");
+  //   await page.getByText("ロゴ ファイルをドラッグ&ドロップ、またはクリックして選択").click();
+  //   const logoFileChooser = await fileChooserPromise;
+  //   await logoFileChooser.setFiles(path.join(path.dirname(fileURLToPath(import.meta.url)), "fixtures/test.jpg"));
+  //   await page.getByRole("textbox", { name: "所在地" }).fill("test_address");
+  //   await page.getByRole("textbox", { name: "サイトURL" }).fill("test@example.com/test_site");
+  //   await page.getByRole("textbox", { name: "従業員数" }).fill("100");
+
+  //   await page.getByRole("button", { name: "更新する" }).click();
+
+  //   await expect(page.getByText("企業名を入力してください。", { exact: true })).toBeVisible();
+
+  //   await page.getByRole("textbox", { name: "会社名" }).fill("test_company_profile_5_edited");
+  //   await page.getByRole("button", { name: "更新する" }).click();
+
+  //   page.on("dialog", async (dialog) => {
+  //     expect(dialog.message()).toContain("プロフィールを更新しました。");
+  //     await dialog.accept();
+  //   });
+
+  //   await expect(page.getByRole("textbox", { name: "会社名" })).toHaveValue("test_company_profile_5_edited");
+  //   await expect(page.getByAltText("プレビュー")).toBeVisible();
+  //   await expect(page.getByRole("textbox", { name: "所在地" })).toHaveValue("test_address");
+  //   await expect(page.getByRole("textbox", { name: "サイトURL" })).toHaveValue("test@example.com/test_site");
+  //   await expect(page.getByRole("textbox", { name: "従業員数" })).toHaveValue("100");
+
+  //   // NOTE: 再びページを訪れると、更新した内容が表示されること
+  //   await page.goto("/profile");
+
+  //   await expect(page.getByRole("textbox", { name: "会社名" })).toHaveValue("test_company_profile_5_edited");
+  //   await expect(page.getByAltText("プレビュー")).toBeVisible();
+  //   await expect(page.getByRole("textbox", { name: "所在地" })).toHaveValue("test_address");
+  //   await expect(page.getByRole("textbox", { name: "サイトURL" })).toHaveValue("test@example.com/test_site");
+  //   await expect(page.getByRole("textbox", { name: "従業員数" })).toHaveValue("100");
+  // });
+});
