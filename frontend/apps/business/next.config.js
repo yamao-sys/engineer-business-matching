853fa07b4bb25c020a19cae01eeb558085f3058b
yamao-sys <yamaopanku@gmail.com>
2025-08-31 12:46:20 +0900

    Businessのnext.config.jsでImageのremotePatterns追加

--- frontend/apps/business/next.config.js
+++ frontend/apps/business/next.config.js
@@ -5,6 +5,22 @@ const nextConfig = {
       bodySizeLimit: "10mb",
     },
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "host.docker.internal",
+        port: "4566",
+        pathname: "/engineer-business-matching-dev/store/**",
+      },
+      {
+        protocol: "http",
+        hostname: "host.docker.internal",
+        port: "4566",
+        pathname: "/engineer-business-matching-test/store/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
