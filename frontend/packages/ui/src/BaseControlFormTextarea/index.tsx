f7e687c03c7b0adecaabcc4cc83666e0a843e0d5
yamao-sys <yamaopanku@gmail.com>
2025-08-31 12:47:10 +0900

    BaseControlFormTextarea: textareaで設定できる項目を受け取れるように

--- frontend/packages/ui/src/BaseControlFormTextarea/index.tsx
+++ frontend/packages/ui/src/BaseControlFormTextarea/index.tsx
@@ -1,4 +1,4 @@
-import { memo } from "react";
+import { JSX, memo } from "react";
 import { Control, Controller, FieldValues, Path } from "react-hook-form";
 import BaseFormTextarea from "../BaseFormTextarea";
 
@@ -8,16 +8,16 @@ type Props<T extends FieldValues> = {
   control: Control<T>;
   name: Path<T>;
   validationErrors: string[];
-};
+} & JSX.IntrinsicElements["textarea"];
 
-function BaseControlFormTextareaInner<T extends FieldValues>({ id, label, control, name, validationErrors }: Props<T>) {
+function BaseControlFormTextareaInner<T extends FieldValues>({ id, label, control, name, validationErrors, ...props }: Props<T>) {
   return (
     <>
       <Controller
         control={control}
         name={name}
         render={({ field }) => (
-          <BaseFormTextarea id={id} label={label} {...field} value={field.value ?? ""} validationErrorMessages={validationErrors} />
+          <BaseFormTextarea id={id} label={label} {...field} value={field.value ?? ""} validationErrorMessages={validationErrors} {...props} />
         )}
       />
     </>
